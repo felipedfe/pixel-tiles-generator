dea8af3fc26b0507f118509397c68260030fbe7e
Felipe Fernandes <felipedfe@gmail.com>
2023-05-03 07:05:04 -0300

    starting project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Pixel from './components/Pixel';
+import { CirclePicker } from 'react-color'
 import './App.css';
 
+const Board = styled.div`
+width: ${(props) => props.boardSize}px;
+`
+
+const Row = styled.div`
+display: flex;
+`
+
 function App() {
+  const [selectedColor, setSelectedColor] = useState("#ba68c8");
+  const boardSize = 400;
+  const numberOfColumnsAndRows = 15;
+
+  const boardGrid = new Array(numberOfColumnsAndRows).fill("");
+  const pixelSize = boardSize / numberOfColumnsAndRows;
+  console.log(pixelSize);
+
+  const colorSet = [
+    '#D9E3F0',
+    '#F47373',
+    '#697689',
+    '#37D67A',
+    '#2CCCE4',
+    '#555555',
+    '#dce775',
+    '#ff8a65',
+    '#ba68c8',
+    '#0c2ef0',
+    '#763ee6',
+    '#000000',
+  ]
+
+  const handleChange = ({ hex }) => {
+    console.log(hex);
+    setSelectedColor(hex)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CirclePicker
+        colors={colorSet}
+        onChange={(e) => handleChange(e)}
+        width='200px'
+        circleSize={45}
+      />
+
+      <Board boardSize={boardSize}>
+        {
+          boardGrid.map(() => {
+            return (
+              <Row>
+                {boardGrid.map(() => <Pixel selectedColor={selectedColor} pixelSize={pixelSize} />)}
+              </Row>
+            )
+          })
+        }
+      </Board>
+    </>
   );
 }
 
---
+++ src/components/Pixel.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledPixel = styled.button`
+  width: ${(props) => props.pixelSize}px;
+  height: ${(props) => props.pixelSize}px;
+  background-color: ${(props) => props.currentColor};
+  border: solid gray 1px;
+  &:active {
+    background-color: aqua;
+  }
+`
+
+
+function Pixel({ pixelSize, selectedColor }) {
+  const [currentColor, setCurrentColor] = useState("#ffffff");
+
+  return (
+    <StyledPixel
+      onClick={() => setCurrentColor(selectedColor)}
+      currentColor={currentColor}
+      pixelSize={pixelSize}
+    />
+
+  )
+};
+
+export default Pixel;
